ea20034c9b34a86d01e4fcb87886d03066acc1ef
Charan-HA <charanha2001charu@gmail.com>
2025-04-25 00:24:06 +0530

    commit 1

---
+++ hiber2/src/main/java/practice/hiber2/App.java
@@ -0,0 +1,23 @@
+package practice.hiber2;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Configuration cfg = new Configuration();
+        cfg.configure("hibernate.cfg.xml");
+        SessionFactory sf = cfg.buildSessionFactory();
+        Session sess= sf.openSession();
+        
+        sess.beginTransaction();
+        
+        Employee emp = new Employee("Akhul", 40000, "Assosiate", "Tester");
+        sess.persist(emp);
+        
+        sess.getTransaction().commit();
+    }
+}
